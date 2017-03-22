13d835871e537f3fa0508c9d3a8063ceee1faf2c
Preston Williams <prwilliams@sherwin.com>
2017-03-22 15:24:15 -0400

    Added font file

--- app.js
+++ app.js
@@ -6,3 +6,26 @@ rgbToHsl(r, g, b)
 
 
 */
+
+"use strict"
+
+var inDetailView = false;
+
+$(document).ready( function(){
+    var appWrapper = $( '#perfect-app-wrapper' );
+    appWrapper.click( function(){
+        if ( !inDetailView ) {
+            appWrapper.addClass( 'inDetailView' );
+            window.setTimeout( function(){
+                appWrapper.addClass( 'transitionView' );
+            }, 2050);
+            inDetailView = true;
+        } else {
+            appWrapper.removeClass( 'inDetailView' );
+            window.setTimeout( function(){
+                appWrapper.removeClass( 'transitionView' );
+            }, 2050);
+            inDetailView = false;
+        }
+    })
+});
